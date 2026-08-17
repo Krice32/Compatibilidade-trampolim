cc15205b9aaad83cffd3b762c55660ac68b4f2e5
Krice32 <erickrocha32323@hotmail.com>
2026-08-17 19:50:23 -0300

    Aplicando ajustes de config

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/Krice32/Compatibilidade-trampolim', 
-})
-export default defineConfig({
-  plugins: [react()],
+  base: '/Compatibilidade-trampolim/',
 })
